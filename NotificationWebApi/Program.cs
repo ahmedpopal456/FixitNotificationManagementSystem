3edfe3bf221f4e30ceb15b8861f62472c50b5e90
a-paggabao <a.paggabao@hotmail.com>
2020-11-04 20:59:59 -0500

    Update print message

--- NotificationWebApi/Program.cs
+++ NotificationWebApi/Program.cs
@@ -6,7 +6,7 @@ namespace NotificationWebApi
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World !");
+      Console.WriteLine("Hello Fixit World!!");
     }
 
     public bool DummyFunction()
