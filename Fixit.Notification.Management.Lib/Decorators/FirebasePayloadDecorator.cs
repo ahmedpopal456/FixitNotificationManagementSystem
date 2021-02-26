24c54adbbd959fb8e05325d281843ee17b9aae98
a-paggabao <a.paggabao@hotmail.com>
2021-02-26 00:56:22 -0500

    #442 Updated FCM payload
    - Removed the body for now
    - Placed the action type as the title

--- Fixit.Notification.Management.Lib/Decorators/FirebasePayloadDecorator.cs
+++ Fixit.Notification.Management.Lib/Decorators/FirebasePayloadDecorator.cs
@@ -8,16 +8,14 @@ namespace Fixit.Notification.Management.Lib.Decorators
 {
 	public class FirebasePayloadDecorator : NotificationBaseDecorator
 	{
-		private readonly string Title = "Fixit";
-
 		public FirebasePayloadDecorator(NotificationDto notificationDto) : base(notificationDto)
 		{
 		}
 
 		public new string GetBase64StringConversion()
 		{
-			var body = ToSentenceCase(notificationDto.Action.ToString());
-			var notification = "\"notification\" : {\"title\":\"" + Title + "\", \"body\":\"" + body + "\"}";
+			var title = ToSentenceCase(notificationDto.Action.ToString());
+			var notification = "\"notification\" : {\"title\":\"" + title + "\", \"body\":\"\"}";
 
 			var message = base.GetBase64StringConversion();
 			var data = "\"data\" : {\"message\":\"" + message + "\"}";
